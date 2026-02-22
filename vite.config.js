8fec043f3cdf1ffabe090f53aaf9bf0ddfcee79b
tchoumi313 <tchouminzikeubd@gmail.com>
2026-02-22 11:33:45 +0100

    feat: update Vite configuration to load environment variables and set backend URL

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        //rewrite: (path),
-      },
-      '/assets': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  const backendUrl = env.VITE_API_URL
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: backendUrl,
+          changeOrigin: true,
+        },
+        '/assets': {
+          target: backendUrl,
+          changeOrigin: true,
+        },
       },
     },
-  },
+  }
 })
