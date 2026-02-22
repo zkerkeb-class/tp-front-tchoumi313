ad9f63d58876302f05d2ac9feaa8b67f4db262c0
tchoumi313 <tchouminzikeubd@gmail.com>
2026-02-22 12:20:30 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -22,7 +22,7 @@ export default defineConfig(({ mode }) => {
     preview: {
       host: true,
       port: parseInt(process.env.PORT) || 5173,
-      allowedHosts: "tp-front-tchoumi313.onrender.com"
+      allowedHosts: ['tp-front-tchoumi313.onrender.com']
     },
   }
 })
